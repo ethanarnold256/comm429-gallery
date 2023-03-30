09884345cf7374c44a131e5a1473d3c58e5362ba
ethanarnold256 <96396576+ethanarnold256@users.noreply.github.com>
2023-03-30 14:10:56 -0400

    dynamic routing fixed
    wrong variable name fixed
    all pages used to load placeholder art
    now correctly redirect

--- src/routes/Gallery/[id]/+page.server.js
+++ src/routes/Gallery/[id]/+page.server.js
@@ -1,6 +1,4 @@
-const id = 100330;
-
-export async function load({ fetch }) {
+export async function load({ params }) {
     const url = `https://api.artic.edu/api/v1/artworks/${id}?fields=id,image_id,title,artist_title,date_display,medium_display`;
     const req = await fetch(url);
     const res = await req.json();
